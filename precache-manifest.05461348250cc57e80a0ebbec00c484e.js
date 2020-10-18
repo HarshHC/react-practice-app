524433d9553c6e4f70356d13fa78e36bb2204da6
Harsh Chandra <harshchandrahc5@gmail.com>
2020-10-18 13:55:20 +0100

    Updates

---
+++ precache-manifest.05461348250cc57e80a0ebbec00c484e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e08600fa9afeb60cd6bbfc5a083429",
+    "url": "/react-practice-app/index.html"
+  },
+  {
+    "revision": "10250c6c9fb7e0161bdf",
+    "url": "/react-practice-app/static/css/main.ae377117.chunk.css"
+  },
+  {
+    "revision": "c44e24b309250cc82aaa",
+    "url": "/react-practice-app/static/js/2.ccf610f6.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/react-practice-app/static/js/2.ccf610f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10250c6c9fb7e0161bdf",
+    "url": "/react-practice-app/static/js/main.fa97b127.chunk.js"
+  },
+  {
+    "revision": "2b2f593c500545a15065",
+    "url": "/react-practice-app/static/js/runtime-main.517da115.js"
+  }
+]);
